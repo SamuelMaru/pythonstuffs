a8ca9ca5ddcf25e8480abb9fc88c9295cf94d3d1
Sam <95887362+SamuelMaru@users.noreply.github.com>
2022-03-03 22:35:53 +0000

    Update cipher_make.py
    shorter

--- cipher_make.py
+++ cipher_make.py
@@ -39,27 +39,4 @@ def decipher(text, shift=0):
     :param shift: must be an integer
     :return: n/a
     """
-    if shift > 25:
-        shift %= 26
-    shift = 0-shift
-    parts = []
-    for i in text:
-        if i.islower():
-            if 97 <= ord(i) + shift <= 122:
-                parts.append(chr(ord(i) + shift))
-            elif 97 > ord(i) + shift:
-                parts.append(chr(122 - (97 - (ord(i) + shift))))
-            else:
-                parts.append(chr(97 - (ord(i) - 122)))
-        if i.isupper():
-            if 65 <= ord(i) + shift <= 90:
-                parts.append(chr(ord(i) + shift))
-            elif 65 > ord(i) + shift:
-                parts.append(chr(91 - (64 - (ord(i) + shift))))
-            else:
-                parts.append(chr(65 - (ord(i) - 90)))
-        elif i ==" ":
-            parts.append(i)
-        elif i not in [chr(ele) for ele in range(65,91)]+[chr(ele) for ele in range(97,123)]:
-            raise ValueError("Invalid characters.")
-    return "".join(parts)
+    return encipher(text,(0-shift))
