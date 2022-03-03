fb535a8ddf9a4b925f81612fa91d22dfdd66d185
Sam <95887362+SamuelMaru@users.noreply.github.com>
2022-03-03 17:29:49 +0000

    Update cipher_make.py

--- cipher_make.py
+++ cipher_make.py
@@ -1,9 +1,36 @@
-def encrypt(text, shift=0):
+def encipher(text, shift=0):
     """
     :param text: must be made up of characters in the english alphabet
     :param shift: must be an integer
     :return: n/a
     """
+    parts = []
+    for i in text:
+        if i.islower():
+            if 97 <= ord(i) + shift <= 122:
+                parts.append(chr(ord(i) + shift))
+            elif 97 > ord(i) + shift:
+                parts.append(chr(122 - (97 - (ord(i) + shift))))
+            else:
+                parts.append(chr(97 - (ord(i) - 122)))
+        if i.isupper():
+            if 65 <= ord(i) + shift <= 90:
+                parts.append(chr(ord(i) + shift))
+            elif 65 > ord(i) + shift:
+                parts.append(chr(91 - (64 - (ord(i) + shift))))
+            else:
+                parts.append(chr(65 - (ord(i) - 90)))
+        elif i not in [chr(ele) for ele in range(65,91)]+[chr(ele) for ele in range(97,123)]:
+            raise ValueError("Invalid characters.")
+    return "".join(parts)
+
+def decipher(text, shift=0):
+    """
+    :param text: must be made up of characters in the english alphabet
+    :param shift: must be an integer
+    :return: n/a
+    """
+    shift = 0-shift
     parts = []
     for i in text:
         if i.islower():
