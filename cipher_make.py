12ce829f7122f4ffdc60efa2e0dd37b1f59cac91
Sam <95887362+SamuelMaru@users.noreply.github.com>
2022-03-04 19:16:15 +0000

    Update cipher_make.py

--- cipher_make.py
+++ cipher_make.py
@@ -1,8 +1,47 @@
 """
 Caesar Cipher enciphering and deciphering
 """
+import string
+
 
 def encipher(text, shift=0):
+    all_letters = [chr(ele) for ele in range(65, 91)] + [
+        chr(ele) for ele in range(97, 123)
+    ]
+    punctuation = [
+        "!",
+        '"',
+        "#",
+        "$",
+        "%",
+        "&",
+        "'",
+        "(",
+        ")",
+        "*",
+        "+",
+        ",",
+        "-",
+        ".",
+        "/",
+        ":",
+        ";",
+        "<",
+        "=",
+        ">",
+        "?",
+        "@",
+        "[",
+        "\\",
+        "]",
+        "^",
+        "_",
+        "`",
+        "{",
+        "|",
+        "}",
+        "~",
+    ]
     """
     :param text: must be made up of characters in the english alphabet
     :param shift: must be an integer
@@ -10,25 +49,25 @@ def encipher(text, shift=0):
     """
     if shift > 25:
         shift %= 26
-    parts = []
+    parts = ""
     for i in text:
         if i.islower():
             if 97 <= ord(i) + shift <= 122:
-                parts.append(chr(ord(i) + shift))
+                parts += chr(ord(i) + shift)
             elif 97 > ord(i) + shift:
-                parts.append(chr(122 - (97 - (ord(i) + shift))))
+                parts += chr(122 - (97 - (ord(i) + shift)))
             else:
-                parts.append(chr(97 - (ord(i) - 122)))
+                parts += chr(97 - (ord(i) - 122))
         if i.isupper():
             if 65 <= ord(i) + shift <= 90:
-                parts.append(chr(ord(i) + shift))
+                parts += chr(ord(i) + shift)
             elif 65 > ord(i) + shift:
-                parts.append(chr(91 - (64 - (ord(i) + shift))))
+                parts += chr(91 - (64 - (ord(i) + shift)))
             else:
-                parts.append(chr(65 - (ord(i) - 90)))
-        elif i ==" ":
-            parts.append(i)
-        elif i not in [chr(ele) for ele in range(65,91)]+[chr(ele) for ele in range(97,123)]:
+                parts += chr(65 - (ord(i) - 90))
+        elif i in punctuation:
+            parts += i
+        elif i not in all_letters:
             raise ValueError("Invalid characters.")
     return "".join(parts)
 
@@ -39,4 +78,4 @@ def decipher(text, shift=0):
     :param shift: must be an integer
     :return: n/a
     """
-    return encipher(text,(0-shift))
+    return encipher(text, (0 - shift))
